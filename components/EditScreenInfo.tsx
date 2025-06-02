7554c9763ef6e13e8f7ecb2d48f16017d0929edb
Antek Nguyen Manh <ak@Anteks-Mac-mini.local>
2025-06-02 16:25:33 +0100

    all

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,29 +1,40 @@
-import { Text, View } from 'react-native';
+import { Pressable, Text, TextInput, View } from 'react-native';
 
 export const EditScreenInfo = ({ path }: { path: string }) => {
-  const title = 'Open up the code for this screen:';
-  const description =
-    'Change any of the text, save the file, and your app will automatically update.';
+  const txt = 'Email';
+  const pass = 'Password';
+  const signIn = 'Sign In';
+  const forgotPass = 'Forgot Password?';
 
   return (
     <View>
       <View className={styles.getStartedContainer}>
-        <Text className={styles.getStartedText}>{title}</Text>
-        <View className={styles.codeHighlightContainer + styles.homeScreenFilename}>
-          <Text>{path}</Text>
-        </View>
-        <Text className={styles.getStartedText}>{description}</Text>
+        <Text className={styles.getStartedText}>{txt}</Text>
+        <TextInput className={styles.inputArea} placeholder='Email address'></TextInput>
+        <Text className={styles.getStartedText}>{pass}</Text>
+        <TextInput className={styles.inputArea} placeholder='Password' secureTextEntry={true}></TextInput>
+        <Pressable className={styles.buttonSignIn} onPress={() => console.log('Sign In Pressed')}>
+          <Text className={styles.signIn}>{signIn}</Text>
+        </Pressable>
       </View>
+      <View className={styles.helpContainer}>
+        <Pressable className={styles.helpLink} onPress={() => console.log('Forgot Password Pressed')}>
+          <Text className={styles.helpLinkText}>{forgotPass}</Text>
+        </Pressable>
+        </View>
     </View>
   );
 };
 
 const styles = {
   codeHighlightContainer: `rounded-md px-1`,
-  getStartedContainer: `items-center mx-12`,
-  getStartedText: `text-lg leading-6 text-center`,
+  getStartedContainer: `justify-items-normal mx-12`,
+  getStartedText: `text-lg leading-6 text-left pt-3`,
   helpContainer: `items-center mx-5 mt-4`,
   helpLink: `py-4`,
   helpLinkText: `text-center`,
   homeScreenFilename: `my-2`,
+  inputArea: 'border border-black-600 rounded-md p-1 w-80 h-10',
+  buttonSignIn: 'justify-items-center bg-sky-500 rounded-2xl p-2 mt-3 w-60',
+  signIn: 'text-center text-white',
 };
