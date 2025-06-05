3ee161299e090ef310db3c488e1c20be685d0e99
jjbsnell <jack.jjs.snell@gmail.com>
2025-06-05 10:58:20 +0100

    set up blank pages

---
+++ pages/AddItemScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function AddItemScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-xl">Add Item Screen</Text>
+    </View>
+  );
+}
---
+++ pages/UserScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function UserScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-xl">User Profile Screen</Text>
+    </View>
+  );
+}
