0c08d4272bb4d067594dc7793b76c5783908ab56
jjbsnell <jack.jjs.snell@gmail.com>
2025-06-09 16:15:14 +0100

    find food templates

--- App.tsx
+++ App.tsx
@@ -4,13 +4,13 @@ import { StatusBar } from 'expo-status-bar';
 
 import { HomeScreen } from './pages/HomeScreen';
 import LoginScreen from './pages/LoginScreen';
-
+import FindFoodScreen from './pages/FindFoodScreen';
 import RecipeFilterScreen from './pages/RecipeFilterScreen';
 import RecipeResultScreen from './pages/RecipeResultScreen';
 import InventoryScreen from './pages/InventoryScreen';
 import AddItemScreen from './pages/AddItemScreen';
 import UserScreen from './pages/UserScreen';
-
+import UploadToFindFoodScreen from './pages/UploadToFindFoodScreen';
 import './global.css';
 import { UserProvider } from 'contexts/UserContext';
 import CreateAccountScreen from 'pages/CreateAccountScreen';
@@ -30,6 +30,8 @@ export default function App() {
           <Stack.Screen name="Inventory" component={InventoryScreen} />
           <Stack.Screen name="AddItem" component={AddItemScreen} />
           <Stack.Screen name="User" component={UserScreen} />
+          <Stack.Screen name="FindFood" component={FindFoodScreen} />
+          <Stack.Screen name="UploadToFindFood" component={UploadToFindFoodScreen} />
         </Stack.Navigator>
       <StatusBar style="auto" />
       </UserProvider>
