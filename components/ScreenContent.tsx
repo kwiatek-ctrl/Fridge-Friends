d7d6a4965dce9b187737f0bf43842692ba0133ca
MarikaGanczarczyk <marikabartman27@gmail.com>
2025-06-11 16:54:17 +0100

    removed grey lines (#45)

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -25,6 +25,6 @@ export const ScreenContent = ({ title, path, children, navigation }: ScreenConte
 
 const styles = {
   container: `items-center flex-1 justify-center`,
-  separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
+  // separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
   title: `text-xl font-bold`,
 };
