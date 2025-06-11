93bc3472f55d8c5249fc1b0d292141e39b91e601
jjbsnell <157059031+jjbsnell@users.noreply.github.com>
2025-06-11 16:39:12 +0100

    remove problem titles from login and create account (#44)

--- pages/LoginScreen.tsx
+++ pages/LoginScreen.tsx
@@ -3,7 +3,7 @@ import { ScreenContent } from 'components/ScreenContent';
 export default function LoginScreen({navigation}) {
   return (
     <ScreenContent
-      title="Sign in to Fridge Friends"
+      
       path="LoginScreen.tsx"
       navigation={navigation}
     />
